20d014968351f986da401f7064745dbd4e614ba1
Paul Stone <pmstone247@gmail.com>
2014-04-08 13:16:01 -0600

    Refactored app to use hashes to store data

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra/base"
 
 class App < Sinatra::Application
 
-  URLS = {}
+  URLS = []
 
   get '/' do
     erb :index
@@ -10,12 +10,15 @@ class App < Sinatra::Application
 
   post '/stats' do
     old_url = params[:old_url]
-    id = URLS.length + 1
-    shortened_url = "http://stagingurl.herokuapp.com/#{id}"
-    URLS = {
-      :old_url => old_url,
-      :shortened_url => shortened_url}
-    erb :stats, :locals => {:old_url => old_url, :shortened_url => shortened_url}
+    id = URLS.count + 1
+    shortened_url = "#{request.base_url}/#{id}"
+    URLS << {:shortened_url => shortened_url, :old_url => old_url}
+    erb :stats, locals: {:old_url => old_url, :shortened_url => shortened_url, :id => id}
   end
-end
 
+  get '/:id' do
+    redirect_page = URLS[0][:old_url]
+    redirect redirect_page
+  end
+
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,15 +3,15 @@ require "capybara/rspec"
 require "spec_helper"
 Capybara.app = App
 
-
-
 feature "URl shortener" do
 
   scenario "It displays a textfield with placeholder and a shorten button" do
     visit '/'
-    fill_in "old_url", :with => "http://www.google.com"
+    fill_in "old_url", :with => "http://paulstone81.blogspot.com"
     click_on "Shorten"
-    expect(page).to have_content("http://www.google.com")
-    expect(page).to have_content("http://stagingurl.herokuapp.com/1")
+    expect(page).to have_content("http://paulstone81.blogspot.com")
+    expect(page).to have_content("example.com/1")
+    visit '/1'
+    expect(current_url).to eq("http://paulstone81.blogspot.com/")
   end
 end
